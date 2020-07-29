821c42378f20b9fdba65c56f21c9afb0b2835236
Kelly Hodis <kelly.hodis@processmaker.com>
2020-07-29 10:24:23 -0700

    Separate HTML and PHP components

---
+++ laracast/separated-components/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$greeting = 'Hello, ';
+
+$name = $_GET['name'];
+
+require 'index.view.php';
---
+++ laracast/separated-components/index.view.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+  <meta charset="UTF-8">
+
+  <title>Document</title>
+
+  <style>
+
+    header {
+
+      background: #F08080;
+
+      padding: 5em;
+
+      text-align: center;
+
+    }
+  </style>
+
+</head>
+
+<body>
+
+  <header>
+
+    <h1>
+
+      <?= $greeting . $name; ?>
+
+    </h1>
+
+  </header>
+
+</body>
+
+</html>
