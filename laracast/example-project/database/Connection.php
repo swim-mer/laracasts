becc6b156845a2abad6509f17ee10bf9c59af3d2
Kelly Hodis <kelly.hodis@processmaker.com>
2020-08-06 16:54:36 -0700

    Add comment

--- laracast/example-project/database/Connection.php
+++ laracast/example-project/database/Connection.php
@@ -2,13 +2,16 @@
 
 class Connection
 {
-  // make method available globally without requiring instance
   public static function make()
   {
     try {
-      $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodos', 'user', 'password');
+      return new PDO('mysql:host=127.0.0.1;dbname=mytodos', 'user', 'password');
     } catch (PDOException $e) {
       die('Could not connect. ' . $e->getMessage());
     }
   }
 }
+
+/* make new connection with:
+    $pdo = Connection::make();
+*/
