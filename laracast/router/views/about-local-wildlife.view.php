2319ca4a76b503de22ade4091af630230800d67c
Kelly Hodis <kelly.hodis@processmaker.com>
2020-08-06 22:34:13 -0700

    Add about-local-wildlife.view.php

---
+++ laracast/router/views/about-local-wildlife.view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+      header {
+        background: #F08080;
+        padding: 5em;
+        text-align: center;
+      }
+    </style>
+</head>
+  <h1>Local Wildlife</h1>
+</body>
+</html>
