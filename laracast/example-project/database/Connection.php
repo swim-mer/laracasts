fdc39a4a5ff444c7672e36f54ab2e539afe50db8
Kelly Hodis <kelly.hodis@processmaker.com>
2020-08-06 17:55:05 -0700

    Integrate with config.php

--- laracast/example-project/database/Connection.php
+++ laracast/example-project/database/Connection.php
@@ -2,10 +2,15 @@
 
 class Connection
 {
-  public static function make()
+  public static function make($config)
   {
     try {
-      return new PDO('mysql:host=127.0.0.1;dbname=mytodos', 'user', 'password');
+      return new PDO(
+        $config['connection'].';dbname='.$config['name'],
+        $config['username'],
+        $config['password'],
+        $config['options']
+      );
     } catch (PDOException $e) {
       die('Could not connect. ' . $e->getMessage());
     }
