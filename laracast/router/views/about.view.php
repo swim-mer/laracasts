ab12b1ec0bfd02ce967be2cbcb9c6cf619a860ca
Kelly Hodis <kelly.hodis@processmaker.com>
2020-08-06 22:09:17 -0700

    Add about.php

---
+++ laracast/router/views/about.view.php
@@ -0,0 +1,20 @@
+<?php
+/*
+// Wrap the files in /example-project into a typical router structure
+//
+//
+*/
+
+require bootstrap.php;
+
+
+
+
+
+
+
+/*
+// make a router for more flexibility
+// usually use a web framework that creates a router for you
+//
+*/
