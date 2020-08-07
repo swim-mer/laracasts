ba755ebb52e1209e6ae4bcf2e05edbc5dd50709f
Kelly Hodis <kelly.hodis@processmaker.com>
2020-08-06 21:44:26 -0700

    Add notes

--- laracast/example-project/config.php
+++ laracast/example-project/config.php
@@ -7,7 +7,9 @@ return [
     'password' => 'password',
     'connection' => 'mysql:host=127.0.0.1',
     'options' => [
+      // Throw exceptions on page 
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      // Display warnings on page
       PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
     ]
   ]
