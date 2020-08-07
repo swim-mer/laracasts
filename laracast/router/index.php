837c83ae547c3a2ef262354afe5b7a75b78f9002
Kelly Hodis <kelly.hodis@processmaker.com>
2020-08-06 22:28:51 -0700

    Add routes

--- laracast/router/index.php
+++ laracast/router/index.php
@@ -1,3 +1,9 @@
 <?php
 
 $database = require 'core/bootstrap.php';
+
+$routes = [
+  '' => 'controllers/index.php',
+  'about' => 'controllers/about.php',
+  'about/local-wildlife' => 'controllers/about-local-wildlife.php',
+]
